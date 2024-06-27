d7272a29534402303008c088490378ba0dd117c2
Ali <malihumayon@gmail.com>
2024-06-27 22:14:52 +0500

    Frontend almost completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 const port = 3000;
 
 const app = express ();
@@ -6,10 +7,28 @@ const app = express ();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.listen(port, ()=>{
     console.log("Running");
 })
 app.get("/", (req, res) =>
 {
     res.render("index");
+})
+
+app.post("/navigation", (req, res) =>
+{
+    if (req.body.action === "viewbooks")
+        {
+            res.redirect("/");
+        }
+        else if (req.body.action === "addbooks")
+            {
+                res.render("add");
+            }
+});
+
+app.post("/add", (req, res)=>
+{
+    console.log(req.body.bookName);
 })
