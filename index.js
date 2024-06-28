78a14eacb873240ec573f3cecd29ec41b65cad35
Ali <malihumayon@gmail.com>
2024-06-28 21:38:58 +0500

    Completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 const port = 3000;
 
 const app = express();
@@ -36,15 +37,16 @@ app.post("/navigation", (req, res) => {
 
 app.post("/add", async (req, res) => {
   let bookName = req.body.bookName;
+  let authorName = req.body.authorName;
   let bookISBN = req.body.bookISBN;
   let dateRead = req.body.dateRead;
   let bookRemarks = req.body.bookRemarks;
   let bookOverview = req.body.bookOverview;
-
+  
   try {
     await db.query(
-      "INSERT INTO BOOKREVIEW (BOOKNAME, ISBN, DATEREAD, REMARKS, OVERVIEW) VALUES ($1, $2, $3, $4, $5)",
-      [bookName, bookISBN, dateRead, bookRemarks, bookOverview]
+      "INSERT INTO BOOKREVIEW (BOOKNAME, ISBN, DATEREAD, REMARKS, OVERVIEW, AUTHOR) VALUES ($1, $2, $3, $4, $5, $6)",
+      [bookName, bookISBN, dateRead, bookRemarks, bookOverview, authorName]
     );
     console.log("Data inserted successfully");
 
@@ -59,6 +61,7 @@ app.post("/add", async (req, res) => {
 async function getDataFromDatabase() {
   const result = await db.query("Select * from bookReview");
   let bookNames = [];
+  let authorNames = [];
   let bookISBNs = [];
   let dateRead = [];
   let bookRemarks = [];
@@ -66,17 +69,44 @@ async function getDataFromDatabase() {
 
   result.rows.forEach((book) => {
     bookNames.push(book.bookname),
+    authorNames.push(book.author);
       bookISBNs.push(book.isbn),
       dateRead.push(book.dateread),
       bookRemarks.push(book.remarks),
       bookOverview.push(book.overview);
+
   });
 
   return {
     bookNames,
+    authorNames,
     bookISBNs,
     dateRead,
     bookRemarks,
     bookOverview,
   };
 }
+
+async function deleteBooks(bookName) {
+  try {
+    await db.query("delete from bookreview where bookname = $1", [bookName]);
+    console.log("Book deleted successfully");
+  } catch {
+    console.log("Error");
+  }
+};
+
+app.post("/delete-book", async(req, res) => 
+{
+    await deleteBooks(req.body.bookName);
+    res.redirect("/");
+})
+
+app.post("/view-notes", async(req, res) =>
+{
+    const data = await getDataFromDatabase();
+    res.render("viewsingle", {
+        bookData: data,
+        count: req.body.counter,
+    });
+});
